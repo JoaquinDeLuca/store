b102c805097bfc5a7a595d27ad4c9794d9610cea
JoaquinDeLuca <joaqui222@gmail.com>
2023-02-24 19:18:17 -0300

    database false

---
+++ database/data.ts
@@ -0,0 +1,61 @@
+const data: Record<TProductId, TProduct> = {
+    '2zd33b8c': {
+      name: 'Maluma Hass Avocado',
+      id: '2zd33b8c',
+      sku: 'NUR72KCM',
+      price: 1.15,
+      image: '/images/maluma.jpg',
+      attributes: {
+        description:
+          'A relatively new cultivar, it was, the pretty boy baby, discovered in South Africa in the early 1990s by Mr. A.G. (Dries) Joubert. Maluma Babyy. It is a chance seedling of unknown parentage',
+        shape: 'Oval',
+        hardiness: '1 °C',
+        taste: 'Catchy, is an avocado',
+      },
+    },
+    '6trfgkkj': {
+      name: 'Fuerte Avocado',
+      id: '6trfgkkj',
+      sku: 'AX4M8SJV',
+      price: 1.21,
+      image: '/images/fuerte.jpg',
+      attributes: {
+        description:
+          'The Fuerte avocado is the second largest commercial variety behind Hass. It is a long time California commercial variety valued for its winter season ripening and its B-Type blossom type which most growers plant adjacent to the Hass for a more consistent production cycle. This avocado tends to produce heavily in alternate years.',
+        shape: 'Pear',
+        hardiness: '-1 °C',
+        taste: 'Magnificent, is a strong avocado',
+      },
+    },
+    '7bcr49em': {
+      name: 'Gwen Hass Avocado',
+      id: '7bcr49em',
+      sku: 'HYA78F6J',
+      price: 1.25,
+      image: '/images/gwen.jpg',
+      attributes: {
+        description:
+          "A seedling bred from 'Hass' x 'Thille' in 1982, 'Gwen' is higher yielding and more dwarfing than 'Hass' in California. The fruit has an oval shape, slightly smaller than 'Hass' (100–200 g or 3.5–7.1 oz), with a rich, nutty flavor. The skin texture is more finely pebbled than 'Hass', and is dull green when ripe. It is frost-hardy down to −1 °C (30 °F)",
+        shape: 'Plump',
+        hardiness: '1 °C',
+        taste: 'Superb, is an avocado',
+      },
+    },
+    '098323ks': {
+      name: 'Bacon Avocado',
+      id: '098323ks',
+      sku: 'BXD100BLK',
+      price: 1.51,
+      image: '/images/bacon.jpg',
+      attributes: {
+        description:
+          'Developed by a farmer, James Bacon, in 1954, Bacon has medium-sized fruit with smooth, green skin with yellow-green, light-tasting flesh. When ripe, the skin remains green, but darkens slightly, and fruit yields to gentle pressure. It is cold-hardy down to −5 °C (23 °F)',
+        shape: 'Oval',
+        hardiness: '5 °C',
+        taste: 'Creamy, is an avocado',
+      },
+    }
+}
+  
+export default data
+  
