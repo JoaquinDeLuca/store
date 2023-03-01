435bf7be9fbaf007e4cbf779a79a65a438efce90
JoaquinDeLuca <joaqui222@gmail.com>
2023-02-28 21:21:54 -0300

    home page products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import Link from 'next/link'
+import api from '../api'
 
 export default function index() {
+
+  const [products, setProducts] = useState<TProduct[]>([])
+
+
+  useEffect(() => {
+    fetch(`${api}/api/watch/`)
+      .then(responde => responde.json())
+      .then(({data, lenght}) => setProducts(data))
+  },[])
+  
   return (
     <div>
         Hi 
         <Link href="/about">
           About
         </Link>
+        <div>
+          {products && products.map(w => (
+            <div key={w.id}>
+              <Link href={`${api}product/${w.id}`}>{w.name}</Link>
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
