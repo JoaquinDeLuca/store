e73cdca1cbca770fa9f92162535512fa55c08f3d
JoaquinDeLuca <joaqui222@gmail.com>
2023-02-28 21:20:29 -0300

    custom document

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head >
+        <link rel='icon' href='https://cdn-icons-png.flaticon.com/512/82/82280.png'/>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
