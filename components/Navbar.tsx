e9da1f811629442742079c07bc443523ee2d942e
JoaquinDeLuca <joaqui222@gmail.com>
2023-03-16 19:29:53 -0300

    the number of added products is displayed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,33 @@
+import Image from 'next/image'
+import Logo from '../public/images/wristwatchNav.png'
+import style from '@styles/navbar.module.css'
+import Link from 'next/link'
+import { TotalQuantity } from '../features/speakerSlice'
+import { RootState } from 'store'
+import { useSelector } from 'react-redux'
+import Cart from './Cart'
 
 export default function Navbar() {
+
+  const quantity = useSelector(TotalQuantity)
+
   return (
-    <div>Navbar</div>
+    <div className={style.container}>
+      <Link className={style.link} href={'/'}>
+        <div className={style.logoAndName}>
+          <Image 
+            src={Logo}
+            width={70}
+            height={70}
+            priority={true}
+            alt="Logo Watch"
+          />
+          <p className={style.p}>Speaker shop</p>
+        </div>
+      </Link>
+      <Link href={"/shoppingCart"} className={style.link}>
+        <p className={style.p}>Cart 🛒{quantity} </p>
+      </Link>
+    </div>
   )
 }
