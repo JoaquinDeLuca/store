508dff8269c3cbfb88c17ef1edcc9ae625f5e6f0
JoaquinDeLuca <joaqui222@gmail.com>
2023-03-20 21:39:09 -0300

    added state of loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,23 @@
-import React,{useEffect, useState} from 'react'
-import Link from 'next/link'
-import api from '../api'
+import React, { useEffect, useState } from "react";
+import api from "../api";
+import Card from "@components/Card";
+import style from "@styles/home.module.css";
 
 export default function index() {
-
-  const [products, setProducts] = useState<TProduct[]>([])
-
+  const [productsList, setProductslist] = useState<TProduct[]>([]);
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     fetch(`${api}/api/watch/`)
-      .then(responde => responde.json())
-      .then(({data, lenght}) => setProducts(data))
-  },[])
-  
+      .then((responde) => responde.json())
+      .then(({ data }: TAPISpeakerResponse) => setProductslist(data))
+      .finally(() => setLoading(false))
+  }, []);
+
   return (
-    <div>
-        Hi 
-        <Link href="/about">
-          About
-        </Link>
-        <div>
-          {products && products.map(w => (
-            <div key={w.id}>
-              <Link href={`${api}product/${w.id}`}>{w.name}</Link>
-            </div>
-          ))}
-        </div>
+    <div className={style.container}>
+      <Card products={productsList} />
     </div>
-  )
+  );
 }
