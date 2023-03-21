8117c10c7f85d53ff4f5e78eb959e41e57a4792e
JoaquinDeLuca <joaqui222@gmail.com>
2023-03-20 21:32:40 -0300

    added svg cart and container new

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,9 @@
-import Image from 'next/image'
-import Logo from '../public/images/wristwatchNav.png'
 import style from '@styles/navbar.module.css'
 import Link from 'next/link'
 import { TotalQuantity } from '../features/speakerSlice'
-import { RootState } from 'store'
 import { useSelector } from 'react-redux'
-import Cart from './Cart'
+import Logo from '/public/images/logo.svg'
+import Cart from '/public/images/cart.svg'
 
 export default function Navbar() {
 
@@ -13,21 +11,17 @@ export default function Navbar() {
 
   return (
     <div className={style.container}>
-      <Link className={style.link} href={'/'}>
-        <div className={style.logoAndName}>
-          <Image 
-            src={Logo}
-            width={70}
-            height={70}
-            priority={true}
-            alt="Logo Watch"
-          />
-          <p className={style.p}>Speaker shop</p>
-        </div>
-      </Link>
-      <Link href={"/shoppingCart"} className={style.link}>
-        <p className={style.p}>Cart 🛒{quantity} </p>
-      </Link>
+      <div className={style.maxwidth}>
+        <Link className={style.link} href={'/'}>
+          <div className={style.logoAndName}>
+            <Logo className={style.logo}/>
+          </div>
+        </Link>
+        <Link href={"/shoppingCart"} className={style.link}>
+          <Cart />
+          <p className={style.totalUnits}>{quantity}</p>
+        </Link>
+      </div>
     </div>
   )
 }
