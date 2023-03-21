a0a85bc9be1d5d10df7cbe1c3fb2970eaacaf7db
JoaquinDeLuca <joaqui222@gmail.com>
2023-03-20 21:35:54 -0300

    added loading spinner

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,13 +1,31 @@
-import { useRouter } from "next/router"
-
+import CardDestails from "@components/CardDestails";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import api from "../../api";
+import style from '@styles/details.module.css';
 
 export default function productID() {
+  const [product, setProduct] = useState<TProduct[]>([]);
+  const [loadding, setLoadding] = useState(true)
 
-   const {query: {id}} = useRouter();
+  const {
+    query: { id },
+  } = useRouter();
+
+  useEffect (() => {
+    setLoadding(true)
+    if(id){
+      fetch(`${api}api/watch/${id}`)
+        .then((response) => response.json())
+        .then(( data : TAPISpeakerDetailResponse)  => setProduct([data]))
+        .finally(() => setLoadding(false));
+    }
+  }, [id]);
+  
+
   return (
-    <div>
-        productID:  {id}
-        ESta es la pagina del producto en si
+    <div className={style.container}>
+      {loadding ? <div className={style.loading}><p>Loading...</p> </div>: <CardDestails product={product}/>}
     </div>
-  )
+  );
 }
