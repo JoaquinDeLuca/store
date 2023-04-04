a98c0d281ea408de10bf6095ebe5e2697f1d06ca
JoaquinDeLuca <joaqui222@gmail.com>
2023-04-04 19:02:46 -0300

    Sing Up User

---
+++ pages/api/auth/signUp.ts
@@ -0,0 +1,63 @@
+import {connectDB} from 'database/db';
+import { NextApiRequest, NextApiResponse } from "next";
+import User from 'models/user'
+import { hash } from 'bcryptjs'
+
+
+connectDB();
+
+export default async function userController(req: NextApiRequest, res : NextApiResponse) {
+
+    const {method, body } = req;
+    const { mail, id, password, photo, logged, name, lastName } = body;
+
+    switch (method) {
+        case "GET":
+            try{
+
+                const users = await User.find()
+                return res.status(200).json(users)
+
+            }catch(error){
+                console.log(error)
+                return res.status(400).json({error})
+            }
+        case "POST":
+            try{
+                if (await User.findOne({mail})){
+                    return res.status(400).json("This email is already associated with a created user");
+                }
+
+                if(password.length < 6) return res.status(400).json("Password must be longer than 6 characters");
+
+                const hashPassword = await hash(password, 12) 
+
+                await User.create({
+                    name,
+                    lastName,
+                    photo, 
+                    mail,
+                    password: hashPassword,
+                    logged
+                })
+
+                return res.status(201).json({ success: true, msg: "user created successfully"});
+                
+            }catch(error){
+                console.log(error);
+                return res.status(400).json({error});
+            }
+        case "DELETE":
+            try{
+                const userDelete = await User.findByIdAndDelete(id)
+                if(!userDelete) return res.status(404).json("User not found");
+                return res.status(200).json("User delete");
+
+            }catch(error){
+                console.log(error)
+                return res.status(400).json({error});
+            }
+        default:
+            return res.status(400).json("this method is not supported")
+    }
+}
